2c61e34550204e106ac41224835ae0cd68bbe574
Grush <gamov.vlad96@mail.ru>
2022-01-03 23:53:36 +0300

    task 2.6 and 2.7 ready

---
+++ first_program/src/main/java/Application26.java
@@ -0,0 +1,12 @@
+import java.util.HashMap;
+
+public class Application26 {
+
+    public static void main (String[] args){
+        String Sent = "Hey... is it   working1?";
+        int words = Sent.split(" +").length;
+        System.out.println(words + " words");
+    }
+}
+
+
