a1a9036e53f2b54eade082e9ee1d27eeab57360e
Grush <gamov.vlad96@mail.ru>
2022-01-05 18:37:21 +0300

    task 2.4 ready

--- first_program/src/main/java/Application24.java
+++ first_program/src/main/java/Application24.java
@@ -3,18 +3,18 @@ public class Application24 {
     public static void main(String[] args){
         boolean checker = false;
         int a = 5; // Длина массива
-        int [] array = new int [a];
-        int N = 15;
-        System.out.print(func(checker,[1, 2, 3, 4, 5], 3));
-
+        System.out.println(func(false,4, new int[] {2, 3, 4, 5})); //false
+        System.out.println(func(false,4, new int[] {2, 2, 3, 4})); //true
+        System.out.println(func(false,6, new int[] {2, 2, 3, 4})); //true
     }
 
-    public static boolean func(boolean checker1, int[] Array, int n){
+    public static boolean func(boolean checker1, int n, int[] Array){
         for (int i = 0; i < Array.length; i++){
             for (int j = i + 1; j < Array.length; j++){
                 if (Array[i] + Array[j] == n) {
                     checker1 = true;
-                    break;}
+                    break;
+                }
             }
         }
         return checker1;
