890d54373fe46d112d85a85fd08d64db97bcf79d
Grush <gamov.vlad96@mail.ru>
2022-01-14 22:39:18 +0300

    First task

---
+++ OOP_project/src/main/java/oop_first/Phone.java
@@ -0,0 +1,67 @@
+package oop_first;
+
+public class Phone {
+
+    int number;
+    String model;
+    double weight;
+
+    public Phone(int number,
+                 String model,
+                 double weight){
+        this(number, model);
+        this.number = number;
+        this.model = model;
+        this.weight = weight;
+    }
+
+    public Phone(int number,
+                 String model){
+        this.number = number;
+        this.model = model;
+    }
+
+    Phone(){
+
+    }
+
+    void receiveCall(String name){
+        System.out.println("Звонит " + name);
+    }
+
+    void receiveCall(String name, int number){
+
+    }
+
+    public int getNumber(){
+        return number;
+    }
+
+    void sendMessage(int ... numbers){
+        for (int number: numbers) {
+            System.out.print(number);
+        }
+    }
+
+    public static void main(String[] args){
+        Phone android = new Phone();
+        Phone iOS = new Phone();
+        Phone windows = new Phone();
+        android.receiveCall();
+        android.getNumber();
+        iOS.receiveCall();
+        iOS.getNumber();
+        windows.receiveCall();
+        windows.getNumber();
+
+        System.out.print(android.number + ' ' + android.model + ' ' + android.weight);
+        System.out.print(iOS.number + ' ' + iOS.model + ' ' + iOS.weight);
+        System.out.print(windows.number + ' ' + windows.model + ' ' + windows.weight);
+
+        receiveCall();
+    }
+
+
+
+
+}
